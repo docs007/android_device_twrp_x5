b99770d89d1c5c4614f4b6a4c1b1bc5a86d030f2
rock12 <s2012srock>
2018-02-08 21:25:23 +0200

    twrp

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/x5.mk \
+    $(LOCAL_DIR)/omni_x5.mk
---
+++ omni_x5.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/doogee/x5/x5.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := x5
+PRODUCT_NAME := omni_x5
+PRODUCT_BRAND := DOOGEE
+PRODUCT_MODEL := X5
+PRODUCT_MANUFACTURER := DOOGEE
